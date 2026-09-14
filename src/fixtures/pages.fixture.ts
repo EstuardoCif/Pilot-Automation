b1eb187096177c3dd696553edde0ee6a208be14c
nelson.cifuentes <nelson.cifuentes@forzalatam.com>
2026-09-14 12:16:44 -0600

    test: login

--- src/fixtures/pages.fixture.ts
+++ src/fixtures/pages.fixture.ts
@@ -1,21 +1,30 @@
 import { test as base, expect } from '@playwright/test';
+
 import { CartPage } from '../pages/cart.page';
 import { CategoryPage } from '../pages/category.page';
+import { LoginPage } from '../pages/login.page';
 import { ShopPage } from '../pages/shop.page';
 
-type PageFixtures = {
+type Pages = {
+  loginPage: LoginPage;
   shopPage: ShopPage;
   categoryPage: CategoryPage;
   cartPage: CartPage;
 };
 
-export const test = base.extend<PageFixtures>({
+export const test = base.extend<Pages>({
+  loginPage: async ({ page }, use) => {
+    await use(new LoginPage(page));
+  },
+
   shopPage: async ({ page }, use) => {
     await use(new ShopPage(page));
   },
+
   categoryPage: async ({ page }, use) => {
     await use(new CategoryPage(page));
   },
+
   cartPage: async ({ page }, use) => {
     await use(new CartPage(page));
   },
---
+++ src/pages/login.page.ts
@@ -0,0 +1,36 @@
+import { expect, Locator, Page } from '@playwright/test';
+import { BasePage } from './base.page';
+
+export class LoginPage extends BasePage {
+  readonly emailInput: Locator;
+  readonly passwordInput: Locator;
+  readonly submitButton: Locator;
+
+  constructor(page: Page) {
+    super(page);
+
+    this.emailInput = page.getByTestId('login-email-input');
+    this.passwordInput = page.getByTestId('login-password-input');
+    this.submitButton = page.getByTestId('login-submit-btn');
+  }
+
+  async open(): Promise<void> {
+    await this.page.goto('/index.php');
+  }
+
+  async login(email: string, password: string): Promise<void> {
+    await this.emailInput.fill(email);
+    await this.passwordInput.fill(password);
+    await this.submitButton.click();
+  }
+
+  async expectLoaded(): Promise<void> {
+    await expect(this.emailInput).toBeVisible();
+    await expect(this.passwordInput).toBeVisible();
+    await expect(this.submitButton).toBeVisible();
+  }
+
+  async expectLoginSuccessful(): Promise<void> {
+    await expect(this.page).toHaveURL(/\/shop\.php(?:[?#].*)?$/);
+  }
+}
